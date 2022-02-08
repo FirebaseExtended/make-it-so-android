dcbdd08fb34305062638071e536aca502f9a0c01
Marina Coelho <marinacoelho16@gmail.com>
2022-02-08 15:26:43 +0000

    Finished repository methods

--- app/src/main/java/com/example/makeitso/model/database/dao/UserDao.kt
+++ app/src/main/java/com/example/makeitso/model/database/dao/UserDao.kt
@@ -5,13 +5,9 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.makeitso.model.User
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
-   @Query("SELECT * FROM User WHERE id = :id")
-    fun select(id: String): Flow<User>
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(user: User)
 
--- app/src/main/java/com/example/makeitso/model/database/repository/UserRepo.kt
+++ app/src/main/java/com/example/makeitso/model/database/repository/UserRepo.kt
@@ -1,7 +1,15 @@
 package com.example.makeitso.model.database.repository
 
+import com.example.makeitso.model.User
 import com.example.makeitso.model.database.dao.UserDao
+import kotlinx.coroutines.Dispatchers
 
 class UserRepo(private val userDao: UserDao) {
+    suspend fun insert(user: User) {
+        Dispatchers.IO.apply { userDao.insert(user) }
+    }
 
+    suspend fun delete(user: User) {
+        Dispatchers.IO.apply { userDao.delete(user.id) }
+    }
 }

7a497971560c9f63d2b39e267b4069b3b0e1713d
Marina Coelho <marinacoelho16@gmail.com>
2022-02-22 11:29:46 +0000

    Added crashlytics to app and recording non fatal exceptions

--- app/src/main/java/com/example/makeitso/model/service/impl/CrashlyticsServiceImpl.kt
+++ app/src/main/java/com/example/makeitso/model/service/impl/CrashlyticsServiceImpl.kt
@@ -1,11 +1,12 @@
 package com.example.makeitso.model.service.impl
 
 import com.example.makeitso.model.service.CrashlyticsService
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
 
 class CrashlyticsServiceImpl @Inject constructor() : CrashlyticsService {
     override suspend fun logNonFatalCrash(throwable: Throwable) {
-        //Log non fatal crash
+        Firebase.crashlytics.recordException(throwable)
     }
-
 }

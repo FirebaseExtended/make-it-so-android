4cf34cc4ea7842c91ce962b3ca52f5bd8b76c7f5
Marina Coelho <marinacoelho16@gmail.com>
2022-04-20 10:34:05 +0100

    Using Firebase colors

--- app/src/main/java/com/example/makeitso/theme/Color.kt
+++ app/src/main/java/com/example/makeitso/theme/Color.kt
@@ -18,6 +18,6 @@ package com.example.makeitso.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BrightOrange = Color(0xFFFC4F4F)
-val MediumOrange = Color(0xFFFF9F45)
-val DarkOrange = Color(0xFFF76E11)
+val BrightOrange = Color(0xFFFF8A65)
+val MediumOrange = Color(0xFFFFA000)
+val DarkOrange = Color(0xFFF57C00)

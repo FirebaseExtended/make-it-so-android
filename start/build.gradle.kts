4456db633e16b5d63a145c33769e034e0cd19e02
rosariopf <rosariopf@google.com>
2023-07-19 14:10:28 +0100

    chore: replace kapt with ksp

--- start/build.gradle.kts
+++ start/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
     id("com.google.dagger.hilt.android") version "2.47" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
     id("com.google.firebase.crashlytics") version "2.9.4" apply false

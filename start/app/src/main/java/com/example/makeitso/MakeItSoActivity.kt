fef41081145906ef0ae3e6c95e9f450b8fce952a
Marina Coelho <marinacoelho16@gmail.com>
2023-08-14 14:49:24 +0100

    Add comment so we remember to update material dependency and experimental annotations when the API stabilizes (#42)

--- start/app/src/main/java/com/example/makeitso/MakeItSoActivity.kt
+++ start/app/src/main/java/com/example/makeitso/MakeItSoActivity.kt
@@ -22,6 +22,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.ExperimentalMaterialApi
 import dagger.hilt.android.AndroidEntryPoint
 
+// MakeItSoActivity starts the first composable, which uses material cards that are still experimental.
+// TODO: Update material dependency and experimental annotations once the API stabilizes.
 @AndroidEntryPoint
 @ExperimentalMaterialApi
 class MakeItSoActivity : AppCompatActivity() {

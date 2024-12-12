711215f76dca6b6052088154bdc8cf3b7c0c8860
Marina Coelho <marinacoelho16@gmail.com>
2024-12-12 10:33:45 +0000

    Add delete account dialog to avoid users accidentally deleting their account

--- v2/app/src/main/java/com/google/firebase/example/makeitso/ui/settings/SettingsScreen.kt
+++ v2/app/src/main/java/com/google/firebase/example/makeitso/ui/settings/SettingsScreen.kt
@@ -5,20 +5,31 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.firebase.example.makeitso.R
 import com.google.firebase.example.makeitso.ui.shared.CenterTopAppBar
 import com.google.firebase.example.makeitso.ui.shared.StandardButton
+import com.google.firebase.example.makeitso.ui.theme.DarkBlue
+import com.google.firebase.example.makeitso.ui.theme.DarkGrey
+import com.google.firebase.example.makeitso.ui.theme.LightRed
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -33,6 +44,10 @@ fun SettingsScreen(
 ) {
     val isAnonymous by viewModel.isAnonymous.collectAsStateWithLifecycle()
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    LaunchedEffect(true) {
+        viewModel.loadCurrentUser()
+    }
 
     Scaffold(
         topBar = {
@@ -52,6 +67,8 @@ fun SettingsScreen(
                     bottom = 4.dp
                 )
         ) {
+            Spacer(Modifier.size(24.dp))
+
             if (isAnonymous) {
                 StandardButton(R.string.sign_in) {
                     openSignInScreen()
@@ -61,12 +78,61 @@ fun SettingsScreen(
                     viewModel.signOut(openHomeScreen)
                 }
 
-                Spacer(Modifier.size(24.dp))
+                Spacer(Modifier.size(16.dp))
 
-                StandardButton(R.string.delete_account) {
-                    viewModel.signOut(openHomeScreen)
-                }
+                DeleteAccountButton(openHomeScreen, viewModel)
             }
         }
     }
+}
+
+@Composable
+fun DeleteAccountButton(
+    openHomeScreen: () -> Unit,
+    viewModel: SettingsViewModel = hiltViewModel()
+) {
+    val showDeleteAccountDialog = remember { mutableStateOf(false) }
+
+    StandardButton(R.string.delete_account) {
+        showDeleteAccountDialog.value = true
+    }
+
+    if (showDeleteAccountDialog.value) {
+        AlertDialog(
+            containerColor = LightRed,
+            textContentColor = DarkBlue,
+            titleContentColor = DarkBlue,
+            title = { Text(stringResource(R.string.delete_account_title)) },
+            text = { Text(stringResource(R.string.delete_account_description)) },
+            dismissButton = {
+                TextButton(
+                    onClick = { showDeleteAccountDialog.value = false },
+                    colors = getDialogButtonColors()
+                ) {
+                    Text(text = stringResource(R.string.cancel), fontSize = 16.sp)
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        showDeleteAccountDialog.value = false
+                        viewModel.deleteAccount(openHomeScreen)
+                    },
+                    colors = getDialogButtonColors()
+                ) {
+                    Text(text = stringResource(R.string.delete), fontSize = 16.sp)
+                }
+            },
+            onDismissRequest = { showDeleteAccountDialog.value = false }
+        )
+    }
+}
+
+private fun getDialogButtonColors(): ButtonColors {
+    return ButtonColors(
+        containerColor = LightRed,
+        contentColor = DarkBlue,
+        disabledContainerColor = LightRed,
+        disabledContentColor = DarkGrey
+    )
 }
